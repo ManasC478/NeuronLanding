68bfe5affdcb33901937688956a51120eaa43b22
ManasC478 <manas.chougule@gmail.com>
2021-11-25 20:19:23 -0800

    change feature text

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,88 +1,98 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
-import HomeIcon from '@material-ui/icons/Home';
-import CreateIcon from '@material-ui/icons/Create';
-import ForumIcon from '@material-ui/icons/Forum';
-import MoneyOffIcon from '@material-ui/icons/MoneyOff';
-import HelpIcon from '@material-ui/icons/Help';
-import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
-import FeatureCarousel from './HelperComponents/FeatureCarousel';
+import HomeIcon from "@material-ui/icons/Home";
+import CreateIcon from "@material-ui/icons/Create";
+import ForumIcon from "@material-ui/icons/Forum";
+import MoneyOffIcon from "@material-ui/icons/MoneyOff";
+import HelpIcon from "@material-ui/icons/Help";
+import LocalLibraryIcon from "@material-ui/icons/LocalLibrary";
+import FeatureCarousel from "./HelperComponents/FeatureCarousel";
 const Features = () => {
-    const [carousel, setCarousel] = useState(window.innerWidth <= 615);
+  const [carousel, setCarousel] = useState(window.innerWidth <= 615);
 
-    useEffect(() => {
-        const setState = () => {
-            setCarousel(window.innerWidth <= 615);
-        }
+  useEffect(() => {
+    const setState = () => {
+      setCarousel(window.innerWidth <= 615);
+    };
 
-        window.addEventListener('resize', setState);
+    window.addEventListener("resize", setState);
 
-        return () => window.removeEventListener('resize', setState);
+    return () => window.removeEventListener("resize", setState);
+  }, []);
 
-    }, [])
-
-
-    return (
-        <section className="features" id="features">
-            <div className="header">
-                <h2>Features</h2>
-                <div id="underline"></div>
+  return (
+    <section className='features' id='features'>
+      <div className='header'>
+        <h2>Features</h2>
+        <div id='underline'></div>
+      </div>
+      <div className='feature-container'>
+        {!carousel ? (
+          <>
+            <div className='feature-item'>
+              <LocalLibraryIcon fontSize={"large"} />
+              <article>
+                <h4>Learn Something New</h4>
+                <p>
+                  Search and explore, there will always be new content for any
+                  subject.
+                </p>
+              </article>
             </div>
-            <div className="feature-container">
-                {
-                    !carousel ?
-                        (
-                            <>
-                                <div className="feature-item">
-                                    <LocalLibraryIcon fontSize={'large'} />
-                                    <article>
-                                        <h4>Learn Something New</h4>
-                                        <p>Search and explore, there will always be new content for any subject.</p>
-                                    </article>
-                                </div>
-                                <div className="feature-item">
-                                    <ForumIcon fontSize={'large'} />
-                                    <article>
-                                        <h4>Interactive Sessions</h4>
-                                        <p>Make new friends and groups by chatting with other users.</p>
-                                    </article>
-                                </div>
-                                <div className="feature-item">
-                                    <MoneyOffIcon fontSize={'large'} />
-                                    <article>
-                                        <h4>Sessions are FREE</h4>
-                                        <p>Everything is free. Watch as many videos as you want for no charge!</p>
-                                    </article>
-                                </div>
-                                <div className="feature-item">
-                                    <HelpIcon fontSize={'large'} />
-                                    <article>
-                                        <h4>Ask Questions</h4>
-                                        <p>When confused ask the vibrant community and receive quality feedback.</p>
-                                    </article>
-                                </div>
-                                <div className="feature-item">
-                                    <CreateIcon fontSize={'large'} />
-                                    <article>
-                                        <h4>Find Practice</h4>
-                                        <p>Find extensive practice sets to perfect what you learned from the videos!</p>
-                                    </article>
-                                </div>
-                                <div className="feature-item">
-                                    <HomeIcon fontSize={'large'} />
-                                    <article>
-                                        <h4>Join a Community</h4>
-                                        <p>Be a NEURON. Join the community and add your own unique impulses to help others.</p>
-                                    </article>
-                                </div>
-                            </>
-                        )
-                        :
-                        <FeatureCarousel />
-                }
+            <div className='feature-item'>
+              <ForumIcon fontSize={"large"} />
+              <article>
+                <h4>Interactive Sessions</h4>
+                <p>Make new friends and groups by chatting with other users.</p>
+              </article>
             </div>
-        </section>
-    )
-}
+            <div className='feature-item'>
+              <MoneyOffIcon fontSize={"large"} />
+              <article>
+                <h4>Sessions are FREE</h4>
+                <p>
+                  Everything is free. Watch as many videos as you want for no
+                  charge!
+                </p>
+              </article>
+            </div>
+            <div className='feature-item'>
+              <HelpIcon fontSize={"large"} />
+              <article>
+                <h4>Ask Questions</h4>
+                <p>
+                  When confused ask the vibrant community and receive quality
+                  feedback.
+                </p>
+              </article>
+            </div>
+            <div className='feature-item'>
+              <CreateIcon fontSize={"large"} />
+              <article>
+                <h4>Find Practice</h4>
+                <p>
+                  Find extensive practice sets to perfect what you learned from
+                  the videos!
+                </p>
+              </article>
+            </div>
+            <div className='feature-item'>
+              <HomeIcon fontSize={"large"} />
+              <article>
+                <h4>Join a Community</h4>
+                <p>
+                  Be a NEURON. Join the community and add your own unique
+                  impulses to help others.
+                </p>
+              </article>
+            </div>
+          </>
+        ) : (
+          <FeatureCarousel />
+        )}
+      </div>
+    </section>
+  );
+};
 
-export default Features
+export default Features;
--- src/components/HelperComponents/FeatureCarousel.js
+++ src/components/HelperComponents/FeatureCarousel.js
@@ -1,84 +1,121 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
-import HomeIcon from '@material-ui/icons/Home';
-import CreateIcon from '@material-ui/icons/Create';
-import ForumIcon from '@material-ui/icons/Forum';
-import MoneyOffIcon from '@material-ui/icons/MoneyOff';
-import HelpIcon from '@material-ui/icons/Help';
-import LocalLibraryIcon from '@material-ui/icons/LocalLibrary';
+import ArrowLeftIcon from "@material-ui/icons/ArrowLeft";
+import ArrowRightIcon from "@material-ui/icons/ArrowRight";
+import HomeIcon from "@material-ui/icons/Home";
+import CreateIcon from "@material-ui/icons/Create";
+import ForumIcon from "@material-ui/icons/Forum";
+import MoneyOffIcon from "@material-ui/icons/MoneyOff";
+import HelpIcon from "@material-ui/icons/Help";
+import LocalLibraryIcon from "@material-ui/icons/LocalLibrary";
 
 const FeatureCarousel = () => {
-    const [x, setX] = useState(0);
-    
-    const goLeft = () => {
-        x === 0 ? setX(-100 * 5) : setX(x + 100);
-    }
+  const [x, setX] = useState(0);
 
-    const goRight = useCallback(() => {
-        x === -100 * 5 ? setX(0) : setX(x - 100);
-    }, [x]);
+  const goLeft = () => {
+    x === 0 ? setX(-100 * 5) : setX(x + 100);
+  };
 
-    useEffect(() => {
-        const automaticSlider = setInterval(() => {
-            goRight();
-        }, 8000);
-        return () => {
-            clearInterval(automaticSlider);
-        }
-    }, [x, goRight])
+  const goRight = useCallback(() => {
+    x === -100 * 5 ? setX(0) : setX(x - 100);
+  }, [x]);
 
-    return (
-        <>
-            <button onClick={goLeft}><ArrowLeftIcon fontSize={'inherit'} /></button>
-            <div className="carousel-container">
-                <div className="feature-item" style={{transform: `translateX(${x}%)`}}>
-                    <LocalLibraryIcon />
-                    <article>
-                        <h4>Learn Comething New</h4>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, nobis!</p>
-                    </article>
-                </div>
-                <div className="feature-item" style={{transform: `translateX(${x}%)`}}>
-                    <ForumIcon />
-                    <article>
-                        <h4>Interactive Sessions</h4>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, nobis!</p>
-                    </article>
-                </div>
-                <div className="feature-item" style={{transform: `translateX(${x}%)`}}>
-                    <MoneyOffIcon />
-                    <article>
-                        <h4>Every Sesssion is FREE</h4>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, nobis!</p>
-                    </article>
-                </div>
-                <div className="feature-item" style={{transform: `translateX(${x}%)`}}>
-                    <HelpIcon />
-                    <article>
-                        <h4>Ask Questions</h4>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, nobis!</p>
-                    </article>
-                </div>
-                <div className="feature-item" style={{transform: `translateX(${x}%)`}}>
-                    <CreateIcon />
-                    <article>
-                        <h4>Find Practice</h4>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, nobis!</p>
-                    </article>
-                </div>
-                <div className="feature-item" style={{transform: `translateX(${x}%)`}}>
-                    <HomeIcon />
-                    <article>
-                        <h4>Join a Community</h4>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, nobis!</p>
-                    </article>
-                </div>
-            </div>
-            <button onClick={goRight}><ArrowRightIcon fontSize={'inherit'} /></button>
-        </>
-    )
-}
+  useEffect(() => {
+    const automaticSlider = setInterval(() => {
+      goRight();
+    }, 8000);
+    return () => {
+      clearInterval(automaticSlider);
+    };
+  }, [x, goRight]);
 
-export default FeatureCarousel
+  return (
+    <>
+      <button onClick={goLeft}>
+        <ArrowLeftIcon fontSize={"inherit"} />
+      </button>
+      <div className='carousel-container'>
+        <div
+          className='feature-item'
+          style={{ transform: `translateX(${x}%)` }}
+        >
+          <LocalLibraryIcon />
+          <article>
+            <h4>Learn Something New</h4>
+            <p>
+              Search and explore, there will always be new content for any
+              subject.
+            </p>
+          </article>
+        </div>
+        <div
+          className='feature-item'
+          style={{ transform: `translateX(${x}%)` }}
+        >
+          <ForumIcon />
+          <article>
+            <h4>Interactive Sessions</h4>
+            <p>Make new friends and groups by chatting with other users.</p>
+          </article>
+        </div>
+        <div
+          className='feature-item'
+          style={{ transform: `translateX(${x}%)` }}
+        >
+          <MoneyOffIcon />
+          <article>
+            <h4>Sessions are FREE</h4>
+            <p>
+              Everything is free. Watch as many videos as you want for no
+              charge!
+            </p>
+          </article>
+        </div>
+        <div
+          className='feature-item'
+          style={{ transform: `translateX(${x}%)` }}
+        >
+          <HelpIcon />
+          <article>
+            <h4>Ask Questions</h4>
+            <p>
+              When confused ask the vibrant community and receive quality
+              feedback.
+            </p>
+          </article>
+        </div>
+        <div
+          className='feature-item'
+          style={{ transform: `translateX(${x}%)` }}
+        >
+          <CreateIcon />
+          <article>
+            <h4>Find Practice</h4>
+            <p>
+              Find extensive practice sets to perfect what you learned from the
+              videos!
+            </p>
+          </article>
+        </div>
+        <div
+          className='feature-item'
+          style={{ transform: `translateX(${x}%)` }}
+        >
+          <HomeIcon />
+          <article>
+            <h4>Join a Community</h4>
+            <p>
+              Be a NEURON. Join the community and add your own unique impulses
+              to help others.
+            </p>
+          </article>
+        </div>
+      </div>
+      <button onClick={goRight}>
+        <ArrowRightIcon fontSize={"inherit"} />
+      </button>
+    </>
+  );
+};
+
+export default FeatureCarousel;
