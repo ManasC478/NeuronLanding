f0b998aa808aa457900f6a40ccfca253ed25c467
ManasC478 <manas.chougule@gmail.com>
2021-11-25 18:29:17 -0800

    add Arjun profile

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,69 +1,68 @@
-import React from 'react'
+import React from "react";
 
-import Member from './HelperComponents/Member';
-import DwijenImage from '../public/Dwijen.jpg';
-import AlexiyImage from '../public/Alexiy.jpeg';
-import SohamImage from '../public/Soham.jpg';
-import ManasImage from '../public/Manas.jpg';
+import Member from "./HelperComponents/Member";
+import DwijenImage from "../public/Dwijen.jpg";
+import AlexiyImage from "../public/Alexiy.jpeg";
+import SohamImage from "../public/Soham.jpg";
+import ManasImage from "../public/Manas.jpg";
+import ArjunImage from "../public/Arjun.png";
 
 const Team = () => {
+  const teamMembers = [
+    {
+      image_url: SohamImage,
+      name: "Soham Manoli",
+      position: "Founder",
+      linkedIn: "https://www.linkedin.com/in/soham-manoli-342b4719a/",
+      github: "https://github.com/msoham123",
+    },
+    {
+      image_url: DwijenImage,
+      name: "Dwijen Chawra",
+      position: "Co-Founder",
+      linkedIn: "https://www.linkedin.com/in/dchawra/",
+      github: "https://github.com/dwijenchawra",
+    },
+    {
+      image_url: ManasImage,
+      name: "Manas Chougule",
+      position: "Developer",
+      linkedIn: "https://www.linkedin.com/in/manas-chougule-8529671bb/",
+      github: "https://github.com/ManasC478",
+    },
+    {
+      image_url: AlexiyImage,
+      name: "Alexiy Buynitsky",
+      position: "Developer",
+      linkedIn: "https://www.linkedin.com/in/alexiy-buynitsky-86115b215/",
+      github: "",
+    },
+    {
+      image_url: ArjunImage,
+      name: "Arjun Krishnan",
+      position: "Designer",
+      linkedIn: "",
+      github: "https://github.com/AKris0090",
+    },
+  ];
 
-    const teamMembers = [
-        {
-            image_url: SohamImage,
-            name: "Soham Manoli",
-            position: "Founder",
-            linkedIn: "https://www.linkedin.com/in/soham-manoli-342b4719a/",
-            github: "https://github.com/msoham123"
-        },
-        {
-            image_url: DwijenImage,
-            name: "Dwijen Chawra",
-            position: "Co-Founder",
-            linkedIn: "https://www.linkedin.com/in/dchawra/",
-            github: "https://github.com/dwijenchawra"
-        },
-        {
-            image_url: ManasImage,
-            name: "Manas Chougule",
-            position: "Developer",
-            linkedIn: "https://www.linkedin.com/in/manas-chougule-8529671bb/",
-            github: "https://github.com/ManasC478"
-        },
-        {
-            image_url: AlexiyImage,
-            name: "Alexiy Buynitsky",
-            position: "Developer",
-            linkedIn: "https://www.linkedin.com/in/alexiy-buynitsky-86115b215/",
-            github: ""
-        },
-        {
-            image_url: "",
-            name: "Arjun Krishnan",
-            position: "Designer",
-            linkedIn: "",
-            github: ""
-        }
-    ]
+  return (
+    <section className='team' id='team'>
+      <div className='header'>
+        <h2>Meet Our Team</h2>
+        <div id='underline'></div>
+      </div>
+      <p>
+        Meet our dedicated and commited team. We strive to simplify the process
+        of learning for everyone.
+      </p>
+      <div className='team-container'>
+        {teamMembers.map((member, index) => {
+          return <Member key={index} data={member} />;
+        })}
+      </div>
+    </section>
+  );
+};
 
-    return (
-        <section className="team" id="team">
-            <div className="header">
-                <h2>Meet Our Team</h2>
-                <div id="underline"></div>
-            </div>
-            <p>Meet our dedicated and commited team. We strive to simplify the process of learning for everyone.</p>
-            <div className="team-container">
-                {
-                    teamMembers.map((member, index) => {
-                        return (
-                            <Member key={index} data={member} />
-                        )
-                    })
-                }
-            </div>
-        </section>
-    )
-}
-
-export default Team
+export default Team;
